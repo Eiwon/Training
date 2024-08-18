b9b6abf6830e4477ac4abfa123e8afcd3d0662d0
Eiwon <elang1301@ajou.ac.kr>
2024-08-18 21:34:09 +0900

    10

--- src/solution3/Basic9.java
+++ src/solution3/Basic9.java
@@ -156,4 +156,90 @@ public class Basic9 {
         
         return answer;
     }
+	public int solution9(int chicken) {
+        int answer = 0;
+        int remains = chicken;
+        
+        while(remains > 9){
+            answer += remains / 10;
+            remains = remains / 10 + remains % 10;
+        }
+        
+        return answer;
+    }
+	public String solution10(String[] id_pw, String[][] db) {
+        for(String[] user : db) {
+        	if(user[0].equals(id_pw[0])) {
+        		return (user[1].equals(id_pw[1])) ? "login" : "wrong pw";
+        	}
+        }
+        
+        return "fail";
+    }
+	public int[] solution11(int[][] score) {
+        int[] answer = new int[score.length];
+        Queue<Integer> q = new PriorityQueue<>((a, b) -> {return b - a;});
+        Map<Integer, Integer> rankTable = new HashMap<>();
+        
+        for(int[] sco : score) {
+        	q.add(sco[0] + sco[1]);
+        }
+        
+        int rank = 1, dup = 0;
+        while(q.size() > 0) {
+        	int sum = q.poll();
+        	if(!rankTable.containsKey(sum)) {
+        		rank += dup;
+        		rankTable.put(sum, rank++);
+        		dup = 0;
+        	}else {
+        		dup++;
+        	}
+        }
+        
+        for(int i = 0; i < score.length; i++) {
+        	answer[i] = rankTable.get(score[i][0] + score[i][1]);
+        }
+        
+        return answer;
+    }
+	public int solution12(int a, int b) {
+		while(b % 2 == 0) {
+        	b /= 2;
+        }
+        while(b % 5 == 0) {
+        	b /= 5;
+        }
+        if(b != 1 && a % b != 0){
+            return 2;
+        }
+        
+        return 1;
+    }
+	public int solution13(int n) {
+        int answer = 0, start = 0, end = n, stack = 0;
+        
+        while(start != end) {
+        	for(int i = start + 1; i <= end; i++) {
+        		if(i % 3 == 0) {
+        			stack++;
+        			continue;
+        		}
+        		int a = i;
+        		while(a > 0) {
+        			if(a % 10 == 3) {
+        				stack++;
+        				break;
+        			}
+        			a /= 10;
+        		}
+        	}
+        	start = end;
+        	end += stack;
+        	stack = 0;
+        }
+        answer = end;
+        
+        return answer;
+    }
 }
