462c5aaaf3722e0525aa66b286748fe20bfa01a7
Eiwon <elang1301@ajou.ac.kr>
2024-08-19 21:42:48 +0900

    11

--- src/solution3/Basic9.java
+++ src/solution3/Basic9.java
@@ -242,4 +242,160 @@ public class Basic9 {
         
         return answer;
     }
+	public int solution13(int[] sides) {
+        int answer = 1;
+        // max보다 작으면서, max - min 보단 큰 수
+        // max보다 크면서, max + min보단 작은 수
+        // max랑 같은 수
+        int max = Math.max(sides[0], sides[1]);
+        int min = Math.min(sides[0], sides[1]);
+        for(int i = max - min +1; i < max; i++){
+            answer++;
+        }
+        for(int i = max + 1; i < max + min; i++){
+            answer++;
+        }
+        return answer;
+    }
+	public int solution14(String my_string) {
+        int answer = 0;
+        
+        String number = "";
+        
+        char p;
+        for(int i = 0; i < my_string.length(); i++) {
+        	p = my_string.charAt(i);
+        	if(Character.isDigit(p)) {
+        		number += p;
+        	}else {
+        		if(number.length() > 0) {
+        			answer += Integer.parseInt(number);
+        			number = "";
+        		}
+        	}
+        }
+        if(number.length() > 0) {
+			answer += Integer.parseInt(number);
+			number = "";
+		}
+        
+        return answer;
+    }
+	
+	public int[] solution15(String[] keyinput, int[] board) {
+        int[] answer = new int[2];
+        int limitX = board[0] / 2, limitY = board[1] / 2;
+        for(String input : keyinput) {
+        	if(input.charAt(0) == 'l' && answer[0] - 1 >= limitX * -1) {
+        		answer[0]--;
+        	}else if(input.charAt(0) == 'r' && answer[0] + 1 <= limitX) {
+        		answer[0]++;
+        	}else if(input.charAt(0) == 'u' && answer[1] + 1 <= limitY) {
+        		answer[1]++;
+        	}else if(input.charAt(0) == 'd' && answer[1] - 1 >= limitY * -1) {
+        		answer[1]--;
+        	}
+        }
+        
+        return answer;
+    }
+	public int solution16(int[] numbers) {
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
+    }
+	public int solution17(int[][] dots) {
+        int[] start = dots[0], end = dots[1];
+        for(int[] dot : dots){
+            start[0] = Math.min(start[0], dot[0]);
+            start[1] = Math.min(start[1], dot[1]);
+            end[0] = Math.max(end[0], dot[0]);
+            end[1] = Math.max(end[1], dot[1]);
+        }
+        return (end[0] - start[0]) * (end[1] - start[1]);
+    }
+	public int solution18(String s) {
+        int answer = 0;
+        int recent = 0;
+        for(String command : s.split(" ")) {
+        	if(command.equals("Z")) {
+        		answer -= recent;
+        	}else {
+        		recent = Integer.parseInt(command);
+        		answer += recent;
+        	}
+        }
+        
+        return answer;
+    }
+	public int solution19(String my_string) {
+        int answer = 0;
+        char p;
+        for(int i = 0; i < my_string.length(); i++){
+            p = my_string.charAt(i);
+            if(Character.isDigit(p)) {
+            	answer += Integer.parseInt(String.valueOf(p));
+            }
+        }
+        return answer;
+    }
+	public String solution20(String my_string) {
+        String answer = "";
+        char p;
+        for(int i = 0; i < my_string.length(); i++) {
+        	p = my_string.charAt(i);
+        	if(p != 'a' && p != 'e' && p != 'o' && p != 'i' && p != 'u') {
+        		answer += p;
+        	}
+        }
+        return answer;
+    }
+	public int[] solution21(int n) {
+        int[] answer = {};
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 2; i <= n; i++) {
+        	if(n % i == 0) {
+        		q.add(i);
+        	}
+        	while(n % i == 0) {
+        		n /= i;
+        	}
+        }
+        answer = new int[q.size()];
+        
+        for(int i = 0; i < answer.length; i++) {
+        	answer[i] = q.poll();
+        }
+        
+        return answer;
+    }
+	public int solution22(String[] spell, String[] dic) {
+        int spellNum = spell.length;
+        Map<Character, Integer> origin = new HashMap<>();
+        Map<Character, Integer> target;
+        
+        char p;
+        for(String sp : spell) {
+        	p = sp.charAt(0);
+        	if(origin.containsKey(p)){
+        		origin.put(p, origin.get(p) + 1);
+        	}else origin.put(p, 1);
+        }
+        
+        for(String word : dic) {
+        	if(word.length() != spellNum) {
+        		continue;
+        	}
+        	target = new HashMap<>();
+        	for(int i = 0; i < word.length(); i++) {
+        		p = word.charAt(i);
+        		if(target.containsKey(p)){
+        			target.put(p, target.get(p) + 1);
+            	}else target.put(p, 1);
+        	}
+        	if(origin.equals(target))
+        		return 1;
+        }
+        
+        return 2;
+    }
 }
